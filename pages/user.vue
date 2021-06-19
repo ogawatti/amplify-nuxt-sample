f11c737432b69ca4aaf98c0b74842480d2276814
ogawatti <ogawattim@gmail.com>
2021-06-20 01:03:50 +0900

    add invoke hello api button to user page if authenticated

--- pages/user.vue
+++ pages/user.vue
@@ -1,8 +1,28 @@
 <template>
   <div class="user">
     <amplify-authenticator>
-      {{ 'authenticated' }}
+      <button @click="onClickHelloButton()">Hello</button>
+      <div v-if="content">
+        {{ content }}
+      </div>
       <amplify-sign-out />
     </amplify-authenticator>
   </div>
 </template>
+
+<script>
+import { API } from 'aws-amplify'
+
+export default {
+  data () {
+    return {
+      content: ''
+    }
+  },
+  methods: {
+    async onClickHelloButton () {
+      this.content = await API.get('hello', '/world')
+    }
+  }
+}
+</script>
