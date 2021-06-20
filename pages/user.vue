ed286f7fa5957d1f66088fa8cf621be7b21f76ed
ogawatti <ogawattim@gmail.com>
2021-06-20 23:45:19 +0900

    add invoke url-shortening api button to user page if authenticated

--- pages/user.vue
+++ pages/user.vue
@@ -5,6 +5,12 @@
       <div v-if="content">
         {{ content }}
       </div>
+
+      <br>
+      <button @click="onClickUrlShorteningButton()">URL Shortening</button>
+      <div v-if="urlShortening">
+        {{ urlShortening }}
+      </div>
       <amplify-sign-out />
     </amplify-authenticator>
   </div>
@@ -16,12 +22,23 @@ import { API } from 'aws-amplify'
 export default {
   data () {
     return {
-      content: ''
+      content: '',
+      urlShortening: ''
     }
   },
   methods: {
     async onClickHelloButton () {
       this.content = await API.get('hello', '/world')
+    },
+    async onClickUrlShorteningButton () {
+      console.log('URL Shortening')
+      const init = {
+        body: {
+          original: 'https://ogawatti.net/video/?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=xxxxxxxxxx&X-Amz-Date=xxxxxxxxxx&X-Amz-Expires=xxxxxxxxxx&X-Amz-SignedHeaders=host&X-Amz-Signature=xxxxxxxxxx'
+        }
+      }
+      const { shortening }  = await API.post('URLShortening', '/url-shortening', init)
+      this.urlShortening = shortening
     }
   }
 }
