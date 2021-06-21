7277cd3826ca37e4fb92bd133a3cf31612ecff83
ogawatti <ogawattim@gmail.com>
2021-06-21 16:38:29 +0900

    add issue presigned url for test.mp4

--- pages/user.vue
+++ pages/user.vue
@@ -11,19 +11,35 @@
       <div v-if="urlShortening">
         {{ urlShortening }}
       </div>
+
+      <br>
+      <button @click="onClickS3PresignUrl()">Presigned URL</button>
+      <div v-if="presignedUrl">
+        {{ presignedUrl }}
+      </div>
+
       <amplify-sign-out />
     </amplify-authenticator>
   </div>
 </template>
 
 <script>
-import { API } from 'aws-amplify'
+import { API, Storage } from 'aws-amplify'
 
 export default {
   data () {
     return {
       content: '',
-      urlShortening: ''
+      urlShortening: '',
+      presignedUrl: ''
+    }
+  },
+  computed: {
+    objectKey () {
+      return 'test.mp4'
+    },
+    expires () {
+      return 604800  // 7 Days
     }
   },
   methods: {
@@ -39,6 +55,9 @@ export default {
       }
       const { shortening }  = await API.post('URLShortening', '/url-shortening', init)
       this.urlShortening = shortening
+    },
+    async onClickS3PresignUrl () {
+      this.presignedUrl = await Storage.get(this.objectKey, { expires: this.expires })
     }
   }
 }
